f9e8d79d429311694ef7530bc975259d2a4407bb
shrabon <getshrabon@gmail.com>
2024-09-26 18:02:49 +0600

    Design Complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
+
+	// Header
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$(".navbar").addClass("header-bg");
+		} else {
+			$(".navbar").removeClass("header-bg");
+		}
+	})
 	$(".testimonial-carousel").owlCarousel({
 		loop: true,
 		margin: 20,
 		nav: false,
 		dots: false,
-		items: 1, 
+		items: 1,
 		// autoplay: true,
 		// autoplayTimeout: 5000,
 		responsive: {
@@ -27,5 +38,11 @@ $(document).ready(function () {
 	});
 	$("#testimonial-next").click(function () {
 		$(".testimonial-carousel").trigger("next.owl.carousel");
+	});
+
+	// Back to top Button
+	$("#back-to-top").click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 800); // Smooth scroll to top over 800ms
+		return false;
 	});
 });
